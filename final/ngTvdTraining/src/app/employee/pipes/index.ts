b17e16c4bfa7918c8d67f645f4a5b777d0d3f039
Thomas Gassmann <thomas.gassmann@windowslive.com>
2018-02-25 17:05:56 +0100

    Pipe Demo Added

--- final/ngTvdTraining/src/app/employee/pipes/index.ts
+++ final/ngTvdTraining/src/app/employee/pipes/index.ts
@@ -1,3 +1,5 @@
+import { EmployeeFilterPipe } from './employee-filter.pipe';
 
-export const pipes: any[] = [];
+export const pipes: any[] = [EmployeeFilterPipe];
 
+export * from './employee-filter.pipe';
