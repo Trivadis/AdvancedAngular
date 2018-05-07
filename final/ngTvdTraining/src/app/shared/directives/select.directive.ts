62fa15e0484d42ef1bf0ec904e53a9680f9f93f9
Thomas Gassmann <thomas.gassmann@windowslive.com>
2018-05-07 13:43:43 +0200

    Change Renderer to Renderer2

--- final/ngTvdTraining/src/app/shared/directives/select.directive.ts
+++ final/ngTvdTraining/src/app/shared/directives/select.directive.ts
@@ -1,19 +1,30 @@
-import { Directive, ElementRef, Renderer, HostListener, HostBinding, Input } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Renderer,
+  HostListener,
+  HostBinding,
+  Input,
+  Renderer2
+} from '@angular/core';
 
 @Directive({ selector: '[appSelect]' })
 export class SelectDirective {
+  @Input() backgroundColor: string;
+
   @HostBinding('class.selected') isSelected = false;
 
-  constructor(private el: ElementRef, private renderer: Renderer) {}
-  @Input() backgroundColor: string;
+  // constructor(private el: ElementRef, private renderer: Renderer) {}
+  constructor(private el: ElementRef, private renderer2: Renderer2) {}
 
   @HostListener('dblclick')
   doubleClick() {
-    this.renderer.setElementStyle(this.el.nativeElement, 'backgroundColor', this.backgroundColor);
+    // this.renderer.setElementStyle(this.el.nativeElement, 'backgroundColor', this.backgroundColor);
+    this.renderer2.setStyle(this.el.nativeElement, 'backgroundColor', this.backgroundColor);
   }
 
-  @HostListener('mouseover')
-  @HostListener('mouseleave')
+  // @HostListener('mouseover')
+  // @HostListener('mouseleave')
   @HostListener('click')
   setSelected() {
     this.isSelected = !this.isSelected;
