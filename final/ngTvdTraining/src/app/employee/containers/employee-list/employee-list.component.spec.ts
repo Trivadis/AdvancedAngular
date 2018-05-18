e97ea4218aa0026021fd28d1ce9287b37edf4819
Thomas Gassmann <thomas.gassmann@windowslive.com>
2018-05-18 10:18:07 +0200

    fix

--- final/ngTvdTraining/src/app/employee/containers/employee-list/employee-list.component.spec.ts
+++ final/ngTvdTraining/src/app/employee/containers/employee-list/employee-list.component.spec.ts
@@ -1,30 +1,33 @@
-import { EmployeeService } from './../../services/employee.service';
-import { RouterTestingModule } from '@angular/router/testing';
-import { TestBed, async, ComponentFixture } from '@angular/core/testing';
-import { EmployeeListComponent } from './employee-list.component';
-import { FormsModule } from '@angular/forms';
-import { EmployeeFilterPipe } from '../../pipes/employee-filter.pipe';
-import { LayoutModule } from '../../../layout/layout.module';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { StoreModule } from '@ngrx/store';
+import { LayoutModule } from '../../../layout/layout.module';
+import { EmployeeFilterPipe } from '../../pipes/employee-filter.pipe';
+import { EmployeeService } from './../../services/employee.service';
+import { reducers } from './../../store';
+import { reducers as reducersFeature } from './../../store/reducers/';
+import { EmployeeListComponent } from './employee-list.component';
 
 describe('EmployeeListComponent', () => {
   let fixture: ComponentFixture<EmployeeListComponent>;
   let comp: EmployeeListComponent;
 
-  beforeEach(
-    async(() => {
-      TestBed.configureTestingModule({
-        imports: [
-          RouterTestingModule,
-          LayoutModule,
-          FormsModule
-        ],
-        providers: [EmployeeService],
-        declarations: [EmployeeListComponent, EmployeeFilterPipe],
-        schemas: [NO_ERRORS_SCHEMA]
-      }).compileComponents();
-    })
-  );
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        LayoutModule,
+        FormsModule,
+        StoreModule.forRoot(reducers),
+        StoreModule.forFeature('employees', reducersFeature)
+      ],
+      providers: [EmployeeService],
+      declarations: [EmployeeListComponent, EmployeeFilterPipe],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EmployeeListComponent);
@@ -32,17 +35,12 @@ describe('EmployeeListComponent', () => {
     fixture.detectChanges();
   });
 
-  it(
-    'should create the app',
-    async(() => {
-      expect(comp).toBeTruthy();
-    })
-  );
+  it('should create the app', async(() => {
+    expect(comp).toBeTruthy();
+  }));
 
-  it('should render title in a h2 tag',
-    async(() => {
-      const result = fixture.nativeElement.querySelector('h2').textContent;
-      expect(result).toContain('Employees');
-    })
-  );
+  it('should render title in a h2 tag', async(() => {
+    const result = fixture.nativeElement.querySelector('h2').textContent;
+    expect(result).toContain('Employees');
+  }));
 });
