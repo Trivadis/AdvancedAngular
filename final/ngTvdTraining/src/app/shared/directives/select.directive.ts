9b6a963fcb0e2b3f5f4a723fd11c18f308b4ea6d
Thomas Gassmann <thomas.gassmann@windowslive.com>
2018-10-26 07:38:00 +0200

    Remove mouseover/mouseleave

--- final/ngTvdTraining/src/app/shared/directives/select.directive.ts
+++ final/ngTvdTraining/src/app/shared/directives/select.directive.ts
@@ -1,19 +1,21 @@
-import { Directive, ElementRef, Renderer, HostListener, HostBinding, Input } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, Renderer } from '@angular/core';
 
 @Directive({ selector: '[appSelect]' })
 export class SelectDirective {
-  @HostBinding('class.selected') isSelected = false;
+  @HostBinding('class.selected')
+  isSelected = false;
 
   constructor(private el: ElementRef, private renderer: Renderer) {}
-  @Input() backgroundColor: string;
+  @Input()
+  backgroundColor: string;
 
   @HostListener('dblclick')
   doubleClick() {
     this.renderer.setElementStyle(this.el.nativeElement, 'backgroundColor', this.backgroundColor);
   }
 
-  @HostListener('mouseover')
-  @HostListener('mouseleave')
+  // @HostListener('mouseover')
+  // @HostListener('mouseleave')
   @HostListener('click')
   setSelected() {
     this.isSelected = !this.isSelected;
