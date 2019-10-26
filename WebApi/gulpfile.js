8e3d230ea797e3c1f72f2fbaa3e9beb16ce28d0d
Francesco Leardini <francesco.leardini@trivadis.com>
2019-10-26 15:48:32 +0200

    Upgraded Gulp to v4. Gulp v3 is not compatible with Node 12.x

--- WebApi/gulpfile.js
+++ WebApi/gulpfile.js
@@ -5,23 +5,23 @@ var gulp = require('gulp'),
 
 var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task('default', ['watch', 'typescript', 'start_node']);
 
-gulp.task('watch', function () {
-    gulp.watch('*.ts', ['start_node']);
-});
-
-gulp.task('typescript', function () {
+gulp.task('typescript', () => {
     console.log('Compile TypeScript to javascript');
 
-    var tsResult = tsProject.src({ base: './' })
-        .pipe(tsProject());
+    var tsResult = tsProject.src().pipe(tsProject());
     return tsResult.js.pipe(gulp.dest('./'));
 });
 
-gulp.task('start_node', ['typescript'], function () {
+gulp.task('start_node', gulp.series('typescript'), () => {
     if (node) {
         node.kill();
     }
     node = spawn('node', ['app.js'], { stdio: 'inherit' });
 });
+
+gulp.task('watch', () => {
+    gulp.watch('*.ts', gulp.series('start_node'));
+});
+
+gulp.task('default', gulp.series('watch', 'typescript', 'start_node'));
